504c6d06ad5f3bae31bee02978533cdad42ca757
vankoosh <vankoosh@users.noreply.github.com>
2022-04-01 20:18:34 -0100

    added some comments for explanation

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ const getGenres = async () => {
   try {
     const response = await fetch(urlToFetch);
     if (response.ok) {
-      const jsonResponse = await response.json();
+      const jsonResponse = await response.json(); // .json() takes the JSON object response that came back from .fetch() promise and converts it into a JS readable object
       console.log(jsonResponse);
-      const genres = jsonResponse.genres;
-      console.log(genres);
+      const genres = jsonResponse.genres; // genres is a jsonResponse object after converting it from a JSON object and here you access its only key "genres" which has an array as its value
+      console.log(genres[0].id); // genres[0].id gives you "28" for example because 0: {id: 28, name: 'Action'}
       return genres;
     }
   } catch (error) {
@@ -22,6 +22,8 @@ const getGenres = async () => {
 
 const getMovies = () => {
   const selectedGenre = getSelectedGenre();
+  console.log(typeof selectedGenre); 
+  // const queryParams = `${tmdbKey}?`
 };
 
 const getMovieInfo = () => {};
